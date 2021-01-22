b51de92c2f4ec26dbb9b41e8eb69bb52d8cb0aed
PaulaCBS <paulasalvado2019@gmail.com>
2021-01-22 12:14:08 -0300

    add dados

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import AllPokemons from './components/AllPokemons/AllPokemons';
 
 const App = () => {
-  const [pokemons, setPokemons] = useState();
+  const [ pokemons, setPokemons ] = useState();
+
+  const[ loading, setLoading ] = useState(true);
+
+  const [ page, setPage ] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=5");
 
   useEffect( () => {
-    getPokemons("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118");
-  }, [] )
+    getPokemons(page); 
+  }, [page] )
 
   const getPokemons = (url) => {
     fetch(url)
       .then( res => res.json())
       .then( data => {
         const pokemonsList = data.results;
-        Promise.all(
+        setPokemons(pokemonsList)
+        setLoading(false)
+        /*Promise.all(
           pokemonsList.map( pokemon => {
             fetch( pokemon.url )
               .then( res => res.json() )
               .then( singlePokemon => setPokemons(singlePokemon) );
           })
-        )
+        )*/
       })
   }
 
   return (
     <Router>
       <div className="App">
-        <Navbar pokemons={pokemons} />
+        <Navbar pokemons={ pokemons } />
+        <AllPokemons pokemons={ pokemons } loading={ loading } />
       </div>
     </Router>
   );
