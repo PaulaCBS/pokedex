958ce3d2916e604bdca13a42950658483fa111be
Nathaly Kaubatz <nathalykaubatz@gmail.com>
2021-01-22 21:32:48 -0300

    Revert "Update App.js"
    This reverts commit f10bae9796be2ac3d3f8e5638f5fa4ec72f4284e.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import AllPokemons from './components/AllPokemons/AllPokemons';
